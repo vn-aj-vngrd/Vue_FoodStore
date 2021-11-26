242552b3a177641b210fbebd1b18207591ad7b7d
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2021-11-27 00:52:16 +0800

    commit

--- src/components/Quantity.vue
+++ src/components/Quantity.vue
@@ -2,7 +2,7 @@
   <div class="d-flex justify-content-between align-items-center">
     <div>
       <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-        <button @click="decrement" type="button" class="btn btn-danger">
+        <button @click="decrement" type="button" class="btn btn-danger btn-sm">
           -
         </button>
         <div class="input-group">
@@ -14,7 +14,7 @@
             v-model="qty"
           />
         </div>
-        <button @click="increment" type="button" class="btn btn-success">
+        <button @click="increment" type="button" class="btn btn-success btn-sm">
           +
         </button>
       </div>
@@ -38,10 +38,8 @@ export default {
   data() {
     return {
       qty: Number,
-      qtyMax: Number,
       total: Number,
       qtyOrig: Number,
-      manualQty: Number,
     };
   },
   created() {
@@ -63,9 +61,12 @@ export default {
       }
     },
     manualInput() {
-      let manualQty = parseInt(this.qty);
-
-      this.$emit("manual-input", manualQty, this.id);
+      if (this.qty < 0) {
+        this.qty = 1;
+      } else if (this.qty > this.qtyMax) {
+        this.qty = this.qtyMax;
+      }
+      this.$emit("manual-input", this.qty, this.id);
     },
   },
 };
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -3,72 +3,13 @@
     <div class="row">
       <div class="col-lg-8 cart">
         <div class="mb-3">
-          <div class="wish-list">
-            <h5 class="mb-4">Cart</h5>
-            <div class="scroll">
-              <div v-if="cartItems.length > 0">
-                <div
-                  class="row mb-4 cart-row"
-                  v-for="item in cartItems"
-                  :key="item.id"
-                >
-                  <div class="col-md-5 col-lg-3 col-xl-3">
-                    <div
-                      class="view zoom overlay z-depth-1 rounded mb-3 mb-md-0"
-                    >
-                      <img
-                        class="img img-thumbnail"
-                        :src="item.img"
-                        alt="Sample"
-                      />
-                    </div>
-                  </div>
-                  <div class="col-md-7 col-lg-9 col-xl-9">
-                    <div>
-                      <div
-                        class="
-                          d-flex
-                          justify-content-between
-                          align-items-center
-                        "
-                      >
-                        <div>
-                          <h5>{{ item.name }}</h5>
-                        </div>
-                        <Button
-                          @on-click="deleteItem"
-                          btnTitle="Remove"
-                          btnStyle="btn btn-outline-danger btn-sm"
-                          :itemId="item.id"
-                        />
-                      </div>
-                      <div>
-                        <p class="mb-3 text-muted text-uppercase small">
-                          {{ `Category: ${item.category}` }} <br />
-                          {{ `Price: PHP ${item.price.toFixed(2)}` }}<br />
-                          {{ `InStock: ${item.max} PCS` }}
-                        </p>
-                      </div>
-                    </div>
-                    <div class="price">
-                      <Quantity
-                        @update-qty="UpdateQty"
-                        @manual-input="manualInput"
-                        :id="item.id"
-                        :quantity="item.quantity"
-                        :max="item.max"
-                        :price="item.price"
-                      />
-                    </div>
-                  </div>
-                  <hr class="mt-4" />
-                </div>
-              </div>
-              <div v-else>
-                <p class="text-muted">The cart is currently empty, please select an item from the menu to check out.</p>
-              </div>
-            </div>
-          </div>
+          <CartItem
+            @manual-input="manualInput"
+            @update-qty="UpdateQty"
+            @delete-item="deleteItem"
+            :cartItems="cartItems"
+            :totalPrice="totalPrice"
+          />
         </div>
       </div>
       <Checkout
@@ -85,9 +26,8 @@
 </template>
 
 <script>
-import Quantity from "../components/Quantity.vue";
+import CartItem from "../components/CartItem.vue";
 import Checkout from "../components/Checkout.vue";
-import Button from "../components/Button.vue";
 
 export default {
   name: "Cart",
@@ -100,9 +40,8 @@ export default {
     isCheckOut: Boolean,
   },
   components: {
-    Quantity,
+    CartItem,
     Checkout,
-    Button,
   },
   methods: {
     manualInput(manualQty, id) {
@@ -132,22 +71,7 @@ export default {
 </script>
 
 <style scoped>
-img {
-  width: 14rem;
-  height: 10rem;
-}
-.scroll {
-  height: 730px;
-  overflow-y: scroll;
-  padding-left: 20px;
-}
-.cart-row {
-  width: 100%;
-}
 .container {
   margin-top: 20px;
-}
-.price {
-  margin-top: 29px;
 }
 </style>
